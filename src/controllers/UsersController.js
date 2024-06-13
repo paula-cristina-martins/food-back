180ade135adf8f5a8bd4202c2bab17d004e21632
Paula Cristina Martins <paula172326@gmail.com>
2024-06-13 10:04:58 -0300

    feat(Usuários): criando controller de usuários - cadastro.

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,37 @@
+const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
+class UsersController {
+	async create(request, response) {
+		const { name, email, password, role } = request.body;
+
+		try {
+			const checkUserExists = await knex("users").where({ email }).first();
+
+			if (checkUserExists) {
+				return response.status(400).json({ error: "E-mail já cadastrado." });
+			}
+
+			const hashedPassword = await hash(password, 8);
+
+			const newUser = {
+				name,
+				email,
+				password: hashedPassword,
+			};
+
+			if (role !== undefined) {
+				newUser.role = role;
+			}
+
+			await knex("users").insert(newUser);
+
+			return response.status(201).json({ message: "Usuário inserido com sucesso!" });
+		} catch (error) {
+			throw new AppError("Erro interno.", 500);
+		}
+	}
+}
+
+module.exports = UsersController;
